7e5ac831263f5957783861786515e8493e2b540f
ishi720 <ishizuka720@gmail.com>
2025-06-20 00:00:54 +0900

    棒をインスタンス化する

--- sketch.js
+++ sketch.js
@@ -1,14 +1,47 @@
+let rotatingLine;
+
 function setup() {
   createCanvas(400, 400);
-  angleMode(DEGREES);
+  angleMode(DEGREES); // 角度は度数法
+
+  // クラスのインスタンス作成
+  rotatingLine = new RotatingLine(
+    200, 200, // 中心点
+    120, // 線の長さ
+    color(255, 0, 0), // 色
+    1, // 時計回り
+    2 // 回転速度
+  );
 }
 
 function draw() {
-  background(220);
-  translate(width / 2, height / 2);
+  background(240);
+  rotatingLine.update();
+  rotatingLine.display();
+}
 
-  rotate(frameCount);
+class RotatingLine {
+  constructor(centerX, centerY, length, color, direction, speed) {
+    this.cx = centerX;
+    this.cy = centerY;
+    this.length = length;
+    this.color = color;
+    this.direction = direction;
+    this.speed = speed;
+    this.angle = 0;
+  }
 
-  stroke(0);
-  line(0, 0, 100, 0);
+  update() {
+    this.angle += this.speed * this.direction;
+  }
+
+  display() {
+    push();
+    translate(this.cx, this.cy);
+    rotate(this.angle);
+    stroke(this.color);
+    strokeWeight(2);
+    line(0, 0, this.length, 0);
+    pop();
+  }
 }
